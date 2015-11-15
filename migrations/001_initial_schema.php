8e09736670e6a48efd21ddb135a4b050a62159c6
programster <stuart.page@irap.org>
2015-11-15 07:27:05 +0000

    moving the database.sql file to be a reasource to be used by the first migration schema

---
+++ migrations/001_initial_schema.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This is an example migration script. Please overwrite the up and down methods.
+ */
+
+class InitialSchema implements iRAP\Migrations\MigrationInterface
+{
+    public function up(\mysqli $mysqliConn) 
+    {
+    }
+    
+    
+    public function down(\mysqli $mysqliConn) 
+    {
+    }
+}
