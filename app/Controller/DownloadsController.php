fe1bb1fdc658afd451c3569db74cd46767651778
programster <stuart.page@irap.org>
2016-02-13 22:24:41 +0000

    formatting DownloadsController

--- app/Controller/DownloadsController.php
+++ app/Controller/DownloadsController.php
@@ -34,14 +34,16 @@
  * "Powered by Orangescrum".
  ********************************************************************************/
 App::uses('AppController', 'Controller');
-class DownloadsController extends AppController {
+
+class DownloadsController extends AppController
+{
     public $name = 'Download';
-	public $components = array('Format');
-	
-	function casefile($files = NULL)
-	{
-		$this->Format->downloadFile($files);
-		exit;
-	}
+    public $components = array('Format');
+    
+    
+    function casefile($files = NULL)
+    {
+        $this->Format->downloadFile($files);
+        exit;
+    }
 }
-?>
