31b1483ce3a15895e28ee78a432454ab3642eb6d
Programster <sdpagent@gmail.com>
2016-02-26 10:06:00 +0000

    formatting the cake test model

--- lib/Cake/TestSuite/Fixture/CakeTestModel.php
+++ lib/Cake/TestSuite/Fixture/CakeTestModel.php
@@ -21,38 +21,39 @@ App::uses('Model', 'Model');
  *
  * @package       Cake.TestSuite.Fixture
  */
-class CakeTestModel extends Model {
-
+class CakeTestModel extends Model 
+{
 	public $useDbConfig = 'test';
 
 	public $cacheSources = false;
 
-/**
- * Sets default order for the model to avoid failing tests caused by
- * incorrect order when no order has been defined in the finds.
- * Postgres can return the results in any order it considers appropriate if none is specified
- *
- * @param integer|string|array $id Set this ID for this model on startup, can also be an array of options, see above.
- * @param string $table Name of database table to use.
- * @param string $ds DataSource connection name.
- */
-	public function __construct($id = false, $table = null, $ds = null) {
+    /**
+     * Sets default order for the model to avoid failing tests caused by
+     * incorrect order when no order has been defined in the finds.
+     * Postgres can return the results in any order it considers appropriate if none is specified
+     *
+     * @param integer|string|array $id Set this ID for this model on startup, can also be an array of options, see above.
+     * @param string $table Name of database table to use.
+     * @param string $ds DataSource connection name.
+     */
+	public function __construct($id = false, $table = null, $ds = null) 
+    {
 		parent::__construct($id, $table, $ds);
 		$this->order = array($this->alias . '.' . $this->primaryKey => 'ASC');
 	}
-
-/**
- * Overriding save() to set CakeTestSuiteDispatcher::date() as formatter for created, modified and updated fields
- *
- * @param array $data
- * @param boolean|array $validate
- * @param array $fieldList
- */
-
-	public function save($data = null, $validate = true, $fieldList = array()) {
+    
+    
+    /**
+     * Overriding save() to set CakeTestSuiteDispatcher::date() as formatter for created, modified and updated fields
+     *
+     * @param array $data
+     * @param boolean|array $validate
+     * @param array $fieldList
+     */
+	public function save($data = null, $validate = true, $fieldList = array()) 
+    {
 		$db = $this->getDataSource();
 		$db->columns['datetime']['formatter'] = 'CakeTestSuiteDispatcher::date';
 		return parent::save($data, $validate, $fieldList);
 	}
-
 }
